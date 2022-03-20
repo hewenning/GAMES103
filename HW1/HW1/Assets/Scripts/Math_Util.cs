2a1cd97ac2af77826020117f0b3df7a52cb4d772
hewenning <919313096@qq.com>
2022-03-20 18:36:27 +0800

    feat: 完成 HW1 的附加作业

--- HW1/HW1/Assets/Scripts/Math_Util.cs
+++ HW1/HW1/Assets/Scripts/Math_Util.cs
@@ -93,5 +93,94 @@ namespace Rigid
             Quaternion q = new Quaternion(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
             return q;
         }
+
+        // Polar Decomposition that returns the rotation from F.
+        public static Matrix4x4 GetRotation(Matrix4x4 F)
+        {
+            Matrix4x4 C = Matrix4x4.zero;
+            for (int ii = 0; ii < 3; ii++)
+                for (int jj = 0; jj < 3; jj++)
+                    for (int kk = 0; kk < 3; kk++)
+                        C[ii, jj] += F[kk, ii] * F[kk, jj];
+
+            Matrix4x4 C2 = Matrix4x4.zero;
+            for (int ii = 0; ii < 3; ii++)
+                for (int jj = 0; jj < 3; jj++)
+                    for (int kk = 0; kk < 3; kk++)
+                        C2[ii, jj] += C[ii, kk] * C[jj, kk];
+
+            float det = F[0, 0] * F[1, 1] * F[2, 2] +
+                            F[0, 1] * F[1, 2] * F[2, 0] +
+                            F[1, 0] * F[2, 1] * F[0, 2] -
+                            F[0, 2] * F[1, 1] * F[2, 0] -
+                            F[0, 1] * F[1, 0] * F[2, 2] -
+                            F[0, 0] * F[1, 2] * F[2, 1];
+
+            float I_c = C[0, 0] + C[1, 1] + C[2, 2];
+            float I_c2 = I_c * I_c;
+            float II_c = 0.5f * (I_c2 - C2[0, 0] - C2[1, 1] - C2[2, 2]);
+            float III_c = det * det;
+            float k = I_c2 - 3 * II_c;
+
+            Matrix4x4 inv_U = Matrix4x4.zero;
+            if (k < 1e-10f)
+            {
+                float inv_lambda = 1 / Mathf.Sqrt(I_c / 3);
+                inv_U[0, 0] = inv_lambda;
+                inv_U[1, 1] = inv_lambda;
+                inv_U[2, 2] = inv_lambda;
+            }
+            else
+            {
+                float l = I_c * (I_c * I_c - 4.5f * II_c) + 13.5f * III_c;
+                float k_root = Mathf.Sqrt(k);
+                float value = l / (k * k_root);
+                if (value < -1.0f) value = -1.0f;
+                if (value > 1.0f) value = 1.0f;
+                float phi = Mathf.Acos(value);
+                float lambda2 = (I_c + 2 * k_root * Mathf.Cos(phi / 3)) / 3.0f;
+                float lambda = Mathf.Sqrt(lambda2);
+
+                float III_u = Mathf.Sqrt(III_c);
+                if (det < 0) III_u = -III_u;
+                float I_u = lambda + Mathf.Sqrt(-lambda2 + I_c + 2 * III_u / lambda);
+                float II_u = (I_u * I_u - I_c) * 0.5f;
+
+
+                float inv_rate, factor;
+                inv_rate = 1 / (I_u * II_u - III_u);
+                factor = I_u * III_u * inv_rate;
+
+                Matrix4x4 U = Matrix4x4.zero;
+                U[0, 0] = factor;
+                U[1, 1] = factor;
+                U[2, 2] = factor;
+
+                factor = (I_u * I_u - II_u) * inv_rate;
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        U[i, j] += factor * C[i, j] - inv_rate * C2[i, j];
+
+                inv_rate = 1 / III_u;
+                factor = II_u * inv_rate;
+                inv_U[0, 0] = factor;
+                inv_U[1, 1] = factor;
+                inv_U[2, 2] = factor;
+
+                factor = -I_u * inv_rate;
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        inv_U[i, j] += factor * U[i, j] + inv_rate * C[i, j];
+            }
+
+            Matrix4x4 R = Matrix4x4.zero;
+            for (int ii = 0; ii < 3; ii++)
+                for (int jj = 0; jj < 3; jj++)
+                    for (int kk = 0; kk < 3; kk++)
+                        R[ii, jj] += F[ii, kk] * inv_U[kk, jj];
+            R[3, 3] = 1;
+            return R;
+        }
+
     }
 }
--- HW1/HW1/Assets/Scripts/Rigid_Bunny_by_Shape_Matching.cs
+++ HW1/HW1/Assets/Scripts/Rigid_Bunny_by_Shape_Matching.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rigid;
+
 
 public class Rigid_Bunny_by_Shape_Matching : MonoBehaviour
 {
@@ -10,9 +12,25 @@ public class Rigid_Bunny_by_Shape_Matching : MonoBehaviour
 	Vector3[] V;
 	Matrix4x4 QQt = Matrix4x4.zero;
 
+	float dt = 0.015f;
+	float linear_decay = 0.999f;
+	float restitution = 0.5f;
+	float u_t = 0.5f;
+	float coeff = 1.3f;
 
-    // Start is called before the first frame update
-    void Start()
+	Vector3[] P = { new Vector3(0, 0.01f, 0), new Vector3(2, 0, 0) };
+	Vector3[] N = { new Vector3(0, 1, 0), new Vector3(-1, 0, 0) };
+
+	// for collision（for penalty)
+	Vector3[] force;
+	//施加力的系数
+	float tho = 10;
+	//开始作用的距离
+	float epsilon = 0.03f;
+
+
+	// Start is called before the first frame update
+	void Start()
     {
     	Mesh mesh = GetComponent<MeshFilter>().mesh;
         V = new Vector3[mesh.vertices.Length];
@@ -50,94 +68,7 @@ public class Rigid_Bunny_by_Shape_Matching : MonoBehaviour
 		transform.rotation=Quaternion.identity;
    	}
 
-   	// Polar Decomposition that returns the rotation from F.
-   	Matrix4x4 Get_Rotation(Matrix4x4 F)
-	{
-		Matrix4x4 C = Matrix4x4.zero;
-	    for(int ii=0; ii<3; ii++)
-	    for(int jj=0; jj<3; jj++)
-	    for(int kk=0; kk<3; kk++)
-	        C[ii,jj]+=F[kk,ii]*F[kk,jj];
-	   
-	   	Matrix4x4 C2 = Matrix4x4.zero;
-		for(int ii=0; ii<3; ii++)
-	    for(int jj=0; jj<3; jj++)
-	    for(int kk=0; kk<3; kk++)
-	        C2[ii,jj]+=C[ii,kk]*C[jj,kk];
-	    
-	    float det    =  F[0,0]*F[1,1]*F[2,2]+
-	                    F[0,1]*F[1,2]*F[2,0]+
-	                    F[1,0]*F[2,1]*F[0,2]-
-	                    F[0,2]*F[1,1]*F[2,0]-
-	                    F[0,1]*F[1,0]*F[2,2]-
-	                    F[0,0]*F[1,2]*F[2,1];
-	    
-	    float I_c    =   C[0,0]+C[1,1]+C[2,2];
-	    float I_c2   =   I_c*I_c;
-	    float II_c   =   0.5f*(I_c2-C2[0,0]-C2[1,1]-C2[2,2]);
-	    float III_c  =   det*det;
-	    float k      =   I_c2-3*II_c;
-	    
-	    Matrix4x4 inv_U = Matrix4x4.zero;
-	    if(k<1e-10f)
-	    {
-	        float inv_lambda=1/Mathf.Sqrt(I_c/3);
-	        inv_U[0,0]=inv_lambda;
-	        inv_U[1,1]=inv_lambda;
-	        inv_U[2,2]=inv_lambda;
-	    }
-	    else
-	    {
-	        float l = I_c*(I_c*I_c-4.5f*II_c)+13.5f*III_c;
-	        float k_root = Mathf.Sqrt(k);
-	        float value=l/(k*k_root);
-	        if(value<-1.0f) value=-1.0f;
-	        if(value> 1.0f) value= 1.0f;
-	        float phi = Mathf.Acos(value);
-	        float lambda2=(I_c+2*k_root*Mathf.Cos(phi/3))/3.0f;
-	        float lambda=Mathf.Sqrt(lambda2);
-	        
-	        float III_u = Mathf.Sqrt(III_c);
-	        if(det<0)   III_u=-III_u;
-	        float I_u = lambda + Mathf.Sqrt(-lambda2 + I_c + 2*III_u/lambda);
-	        float II_u=(I_u*I_u-I_c)*0.5f;
-	        
-	        
-	        float inv_rate, factor;
-	        inv_rate=1/(I_u*II_u-III_u);
-	        factor=I_u*III_u*inv_rate;
-	        
-	       	Matrix4x4 U = Matrix4x4.zero;
-			U[0,0]=factor;
-	        U[1,1]=factor;
-	        U[2,2]=factor;
-	        
-	        factor=(I_u*I_u-II_u)*inv_rate;
-	        for(int i=0; i<3; i++)
-	        for(int j=0; j<3; j++)
-	            U[i,j]+=factor*C[i,j]-inv_rate*C2[i,j];
-	        
-	        inv_rate=1/III_u;
-	        factor=II_u*inv_rate;
-	        inv_U[0,0]=factor;
-	        inv_U[1,1]=factor;
-	        inv_U[2,2]=factor;
-	        
-	        factor=-I_u*inv_rate;
-	        for(int i=0; i<3; i++)
-	        for(int j=0; j<3; j++)
-	            inv_U[i,j]+=factor*U[i,j]+inv_rate*C[i,j];
-	    }
-	    
-	    Matrix4x4 R=Matrix4x4.zero;
-	    for(int ii=0; ii<3; ii++)
-	    for(int jj=0; jj<3; jj++)
-	    for(int kk=0; kk<3; kk++)
-	        R[ii,jj]+=F[ii,kk]*inv_U[kk,jj];
-	    R[3,3]=1;
-	    return R;
-	}
-
+   
 	// Update the mesh vertices according to translation c and rotation R.
 	// It also updates the velocity.
 	void Update_Mesh(Vector3 c, Matrix4x4 R, float inv_dt)
@@ -155,27 +86,84 @@ public class Rigid_Bunny_by_Shape_Matching : MonoBehaviour
 
 	void Collision(float inv_dt)
 	{
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < V.Length; j++)
+			{
+				Vector3 xj = X[j];
+				Vector3 Pxj = xj - P[i];
+				float ProjX = Vector3.Dot(Pxj, N[i]);
+				if (ProjX < epsilon)
+				{
+					float v_n_num = Vector3.Dot(V[j], N[i]);
+					if (v_n_num < 0)
+					{
+						Vector3 v_n = v_n_num * N[i];
+						Vector3 v_t = V[j] - v_n;
+						Vector3 vNnew = -restitution * v_n;
+						float a = Mathf.Max(0, 1 - u_t * (1 + restitution) * v_n.magnitude / v_t.magnitude);
+						Vector3 vTnew = a * v_t;
+						V[j] = vNnew + vTnew;
+						X[j] = X[j] + Mathf.Abs(ProjX) * N[i] * coeff;
+						restitution *= 0.5f;
+					}
+				}
+			}
+		}
 	}
 
     // Update is called once per frame
     void Update()
     {
-  		float dt = 0.015f;
+		if (Input.GetKey("r"))
+		{
+			transform.position = new Vector3(0, 0.6f, 0);
+			restitution = 0.5f;
+			launched = false;
+		}
+		if (Input.GetKey("l"))
+		{
+			launched = true;
+		}
+		if (!launched) return;
 
   		//Step 1: run a simple particle system.
-        for(int i=0; i<V.Length; i++)
+        for(int i = 0; i < V.Length; i++)
         {
-        }
+			V[i] = linear_decay * (V[i] +  MathUtil.gravity * dt);
+			X[i] = X[i] + V[i] * dt;
+		}
 
         //Step 2: Perform simple particle collision.
 		Collision(1/dt);
 
 		// Step 3: Use shape matching to get new translation c and 
 		// new rotation R. Update the mesh by c and R.
-        //Shape Matching (translation)
-		
+		//Shape Matching (translation)
+		Vector3 c = new Vector3(0, 0, 0);
+		for (int i = 0; i < V.Length; i++)
+		{
+			c = c + X[i];
+		}
+		c = c / V.Length;
 		//Shape Matching (rotation)
-		
-		//Update_Mesh(c, R, 1/dt);
-    }
+		Matrix4x4 A = Matrix4x4.zero;
+		for (int i = 0; i < Q.Length; i += 5)
+		{
+			Vector3 Ri = X[i] - c;
+			A[0, 0] += Ri[0] * Q[i][0] * 5;
+			A[0, 1] += Ri[0] * Q[i][1] * 5;
+			A[0, 2] += Ri[0] * Q[i][2] * 5;
+			A[1, 0] += Ri[1] * Q[i][0] * 5;
+			A[1, 1] += Ri[1] * Q[i][1] * 5;
+			A[1, 2] += Ri[1] * Q[i][2] * 5;
+			A[2, 0] += Ri[2] * Q[i][0] * 5;
+			A[2, 1] += Ri[2] * Q[i][1] * 5;
+			A[2, 2] += Ri[2] * Q[i][2] * 5;
+		}
+		A[3, 3] = 1;
+		Matrix4x4 R = MathUtil.GetRotation(A * QQt.inverse);
+
+		Update_Mesh(c, R, 1/dt);
+	}
 }
